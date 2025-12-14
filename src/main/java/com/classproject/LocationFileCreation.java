97f89a56ce88df276dea0f2e075017bae7f5e707
ala-mo <amortensen001@student.sdccd.edu>
2025-12-13 22:58:09 -0800

    Update LocationFileCreation.java

--- src/main/java/com/classproject/LocationFileCreation.java
+++ src/main/java/com/classproject/LocationFileCreation.java
@@ -3,23 +3,13 @@ package com.classproject;
 /**
  * LocationFileCreation
  * --------------------
- * Interface that defines file-related operations for locations.
- * This separates WHAT file actions are required from HOW they are implemented.
- *
- * Demonstrates:
- * - Interfaces
- * - Method declarations
- * - Abstraction
+ * initializing methods 
+     - locationFileCreation(String locationName) ; creates a file for new locations
+     - addingRatingToFile(String locationName, int rating, String review) ; adds ratings and reviews to location files
  */
 public interface LocationFileCreation {
 
-    /**
-     * Creates a file for a new location if it does not already exist.
-     */
     void locationFileCreation(String locationName);
-
-    /**
-     * Appends a rating and review to an existing location file.
-     */
+    
     void addingRatingToFile(String locationName, int rating, String review);
 }
